e1d61533975761661b7635844c4625be0f24ce04
Molly Singmaster <mollysingmaster@gmail.com>
2021-09-10 09:50:10 -0400

    adjusted the code to account for the length of the password

--- script.js
+++ script.js
@@ -2,16 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-var specialChar= ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", 
-":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
-var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
-"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var letterUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
-"Q","R","S","T","U","V","W","X","Y","Z"];
-
-var passwordOption = [];
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -23,21 +13,22 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+
+var specialChar= ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", 
+":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+"Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+
 //prompts user to enter the number of characters in a password
 var stringOfCharacters = prompt(
   "Enter the number of characters in the password between 8-128"
 );
-var numOfPasswords = parseInt(stringOfCharacters);
-
-var lengthOfPass = length.numOfPasswords;
-
-
-//changes the type of stringOfCharacters from string to integer
-numOfCharacters = parseInt(stringOfCharacters);
-
-if (numOfCharacters >= 8 && numOfCharacters <= 128) {
-  
-} else {
+if (stringOfCharacters < 8 || stringOfCharacters > 128) {
   alert("Sorry, your password must be between 8 and 129 characters");
 }
 
@@ -46,42 +37,50 @@ if (numOfCharacters >= 8 && numOfCharacters <= 128) {
 var stringOfLower = confirm(
   "Would you like to include lower case letters?"
 );
-if (stringOfLower) {
-  passwordOptionLo = passwordOption.concat(letterLower);
-}
-
-
-
-
-//asks the user if they want uppercase letters in their password
 var stringOfUpper = confirm(
   "Would you like to include upper case letters?"
 );
-
-if (stringOfUpper) {
-  passwordOptionUp = passwordOption.concat(letterUpper);
-}
-
-
-
-
-//asks the user if they want numbers in their password
-var stringOfNum = confirm("Would you like to include numbers?");
-if (stringOfNum) {
-  passwordOptionNum = passwordOption.concat(num);
-} 
-
-
-
-
-
-//asks the user if they want special characters in their password
+var stringOfNum = confirm(
+  "Would you like to include numbers?"
+  );
 var stringOfSpecial = confirm(
   "Would you like to include special characters? (%, $, @)"
 );
-if (stringOfSpecial) {
-  passwordOptionSp = passwordOption.concat(specialChar);
+
+
+var letterOption = [];
+
+if (stringOfLower === true) { 
+  letterOption = letterLower;
+}
+if (stringOfUpper === true) { 
+  letterOption = letterOption.concat(letterUpper);
+}
+if (stringOfNum === true) { 
+  letterOption = letterOption.concat(num);
+}
+if (stringOfSpecial === true) { 
+  letterOption = letterOption.concat(specialChar);
+}
+else { 
+  noInput()
+}
+
+// Function for no input for character type
+function noInput() {
+  if (letterLower === false && letterUpper === false && 
+    num === false && specialChar === false) {
+  alert("Please choose at least one password criteria")
+    }
 }
 
 
-console.log(lengthOfPass);
+var passwordChoice = []
+// Loop through the letterOption length 
+for (var i = 0; i < passwordLength; i++) {
+  // Get random whole number from index
+  var characterCode = letterOption[Math.floor(Math.random() * letterOption.length)]
+  passwordChoice.push(String.fromCharCode(characterCode))
+}
+// Returns password characters and joins into string
+return passwordChoice.join("")
